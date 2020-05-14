ae715c303904da2a927c4e744131b0e940fe7f5a
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-05-14 17:23:41 -0300

    Update stg_update.groovy

--- stg_update.groovy
+++ stg_update.groovy
@@ -1,5 +1,9 @@
 pipelineJob("stg_update") {
-
+  parameters {
+    stringParam('s3BucketName',"mhplat-nonprod-devops", 'S3 Credentials Bucket')
+    choiceParam('source', ["val"], 'Source Database' )
+    choiceParam('target', ["", "stg", "dev"], 'Target Database' )
+  }
 
   definition {
     cps {
