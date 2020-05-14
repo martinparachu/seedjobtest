8eb67418dd24a7c26ffe4caa4a1d7acf7e2ac55f
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-05-14 20:05:41 -0300

    Update db_update.sh

--- db_update.sh
+++ db_update.sh
@@ -8,39 +8,17 @@ source_owner_user=${source}_owner_user
 
 target_server=10.10.0.183
 target_port=5401
-target_db="mwl_intranet_${target}"
+target_db=mwl_intranet_${target}
 target_owner_password=${target}_owner_password
 target_owner_user=${target}_owner_user
 target_app_password=${target}_app_password
 target_app_user=${target}_app_user
-
-echo "test -----------------------"
-a_2014='hello 2014'
-year=2014
-varname=a_${year}
-echo ${!varname}
-
-varname2=${source}_owner_user
-echo ${!source_owner_user}
-
-
-echo $source_server
-echo $source_port
-echo $source_db
-echo "${!source_owner_user}"
-echo $source_owner_user
-
-echo $target_server
-echo $target_port
-echo $target_db
-echo $target_owner_user
-echo $target_app_user
 
  
 export PGPASSWORD=${!source_owner_password}
 
 pg_dump -h $source_server -p $source_port -U ${!source_owner_user} --no-owner --no-privileges --no-acl -n 'audit' --schema-only -Ft -b -v -f source_dump_audit.tar $source_db
 
-export PGPASSWORD=$target_owner_password
+export PGPASSWORD=${!target_owner_password}
 
-#pg_dump -h $target_server -p $target_port -U $target_owner_user --no-owner --no-privileges --no-acl -n 'queue' --schema-only -Ft -b -v -f target_dump_queue.tar $target_db
+pg_dump -h $target_server -p $target_port -U ${!target_owner_user} --no-owner --no-privileges --no-acl -n 'queue' --schema-only -Ft -b -v -f target_dump_queue.tar $target_db
