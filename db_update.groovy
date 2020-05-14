58d3b8fb46ed47522de4e8ba41882419ef443596
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-05-14 18:20:31 -0300

    Update and rename stg_update.groovy to db_update.groovy

--- db_update.groovy
+++ db_update.groovy
@@ -1,15 +1,15 @@
-pipelineJob("stg_update") {
+pipelineJob("db_update") {
   parameters {
     stringParam('s3BucketName',"mhplat-nonprod-devops", 'S3 Credentials Bucket')
     choiceParam('source', ["val"], 'Source Database' )
     choiceParam('target', ["stg", "dev"], 'Target Database' )
-    booleanParam('RUN_UPDATE', false, 'Check if you really want to proceed')
+    booleanParam('run_update', false, 'Check if you really want to proceed')
   }
 
   definition {
     cps {
       sandbox()
-      script( readFileFromWorkspace('stg_update.jenkinsfile') )
+      script( readFileFromWorkspace('db_update.jenkinsfile') )
     }
   }
 
