38fbbadf838b6fb91de838a518654b7d1666f986
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-05-14 19:33:21 -0300

    Update db_update.sh

--- db_update.sh
+++ db_update.sh
@@ -4,7 +4,7 @@ source_server=10.10.0.183
 source_port=5401
 source_db="mwl_intranet_${source}"
 source_owner_password=${source}_owner_password
-source_owner_user=${source}_owner_user
+source_owner_user=${"${source}_owner_user"}
 
 target_server=10.10.0.183
 target_port=5401
@@ -29,8 +29,8 @@ echo $target_app_user
  
 export PGPASSWORD=$source_owner_password
 
-pg_dump -h $source_server -p $source_port -U $source_owner_user --no-owner --no-privileges --no-acl -n 'audit' --schema-only -Ft -b -v -f source_dump_audit.tar $source_db
+#pg_dump -h $source_server -p $source_port -U $source_owner_user --no-owner --no-privileges --no-acl -n 'audit' --schema-only -Ft -b -v -f source_dump_audit.tar $source_db
 
 export PGPASSWORD=$target_owner_password
 
-pg_dump -h $target_server -p $target_port -U $target_owner_user --no-owner --no-privileges --no-acl -n 'queue' --schema-only -Ft -b -v -f target_dump_queue.tar $target_db
+#pg_dump -h $target_server -p $target_port -U $target_owner_user --no-owner --no-privileges --no-acl -n 'queue' --schema-only -Ft -b -v -f target_dump_queue.tar $target_db
