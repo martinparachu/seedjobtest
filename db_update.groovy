b219ba6933166d6e18783745c851b4b40589dd51
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-06-18 10:53:58 -0300

    Update db_update.groovy

--- db_update.groovy
+++ db_update.groovy
@@ -1,8 +1,6 @@
 pipelineJob("db_update") {
   parameters {
-    stringParam('s3BucketName',"mhplat-nonprod-devops", 'S3 Credentials Bucket')
-    choiceParam('source', ["val"], 'Source Database' )
-    choiceParam('target', ["stg", "dev"], 'Target Database' )
+    choiceParam('target', ["stg"], 'Target Database' )
     booleanParam('run_update', false, 'Check if you really want to proceed')
   }
 
