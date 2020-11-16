825bb03b9919944f13d03ce65d822c728feec00a
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-11-16 18:34:27 -0300

    Update registry_cleanup.sh

--- registry_cleanup.sh
+++ registry_cleanup.sh
@@ -20,7 +20,7 @@ CheckTag(){
         Sha=$(curl -v -s -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X GET http://${REPO}/v2/${Name}/manifests/${Tag} 2>&1 | grep Docker-Content-Digest | awk '{print ($3)}')
         Sha="${Sha/$'\r'/}"
         echo "delete ${Name} ${Tag} ${Sha}"
-        curl -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X DELETE "http://${REPO}/v2/${Name}/manifests/${Sha}"
+        curl -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X DELETE "http://${REPO}/v2/${Name}/manifests/${Sha}" > removed.txt
     fi
 }
 
