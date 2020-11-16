0567a64520146b6d11ce96692eb2815077820687
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-11-16 18:06:23 -0300

    Create registry_cleanup.sh

---
+++ registry_cleanup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+REPO=localhost:5000
+KEEPTAG=3
+SKIPTAG=( development staging validation )
+
+CheckTag(){
+    Name=$1
+    Tag=$2
+
+    Skip=0
+    Match=$(echo "${SKIPTAG[@]}" | grep -o "${Tag}")
+    if [[ ! -z $Match ]]; then
+        Skip=1
+    fi
+	
+    if [[ "${Skip}" == "1" ]]; then
+        echo "skip ${Name} ${Tag}"
+    else
+        Sha=$(curl -v -s -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X GET http://${REPO}/v2/${Name}/manifests/${Tag} 2>&1 | grep Docker-Content-Digest | awk '{print ($3)}')
+        Sha="${Sha/$'\r'/}"
+        echo "delete ${Name} ${Tag} ${Sha}"
+        curl -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X DELETE "http://${REPO}/v2/${Name}/manifests/${Sha}"
+    fi
+}
+
+ScanRepository(){
+    Name=$1
+    echo "Repository ${Name}"
+    curl -s http://${REPO}/v2/${Name}/tags/list | jq '.tags[]' | sort -r | tail -n +$((KEEPTAG+1)) |
+    while IFS=$"\n" read -r line; do
+        line="${line%\"}"
+        line="${line#\"}"
+        CheckTag $Name $line
+    done
+}
+
+
+curl -s http://${REPO}/v2/_catalog | jq '.repositories[]' |
+while IFS=$"\n" read -r line; do
+    line="${line%\"}"
+    line="${line#\"}"
+    ScanRepository $line
+done
