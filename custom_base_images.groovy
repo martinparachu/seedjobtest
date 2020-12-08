712e1ca6cba78e3edee5a060721f879f7646652f
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-12-08 16:46:34 -0300

    Update custom_base_images.groovy

--- custom_base_images.groovy
+++ custom_base_images.groovy
@@ -11,15 +11,7 @@ job("custom-base-images") {
 		choiceParam(
 			"DOCKER_IMAGE",
 			[
-			'dotnet-2-runtime',
-			'dotnet-3.1-runtime',
-			'dotnet-5.0-aspnet',
-			'dotnet-2-runtime-psx',
-      'dotnet-2-sdk-sonarqube',
-      'dotnet-3-sdk-sonarqube',
-      'dotnet-5-sdk-sonarqube',
-			'node-8.15-jessie',
-			'node-12.16.2-buster'
+			'dotnet-3.1-runtime_node'
 			],
 			'Choose the docker image to generate.'
 		)
@@ -28,8 +20,10 @@ job("custom-base-images") {
     scm {
         git {
             remote {
-              url('https://ps-devops@bitbucket.org/primerosystems/ps-devops.git')
-              credentials('bitbucket-credential')
+		    
+	      url('https://github.com/martinparachu/seedjobtest.git')			    
+              //url('https://ps-devops@bitbucket.org/primerosystems/ps-devops.git')
+              //credentials('bitbucket-credential')
             }
             branch('master')
         }
@@ -41,48 +35,18 @@ job("custom-base-images") {
 set -x
 
 case ${DOCKER_IMAGE} in
-"dotnet-2-runtime")
+
+"dotnet-3.1-runtime_node")
   ECR_REPO=dotnet-runtime
-  IMAGE_NAME=dotnet-runtime-2.1-pip-awscli
+  IMAGE_NAME=dotnet-runtime-node-3.1
   ;;
- "dotnet-2-runtime-psx")
-  ECR_REPO=dotnet-runtime
-  IMAGE_NAME=dotnet-runtime-2.1-psx
-  ;;
-"dotnet-2-sdk-sonarqube")
-  ECR_REPO=dotnet-sdk
-  IMAGE_NAME=dotnet-sdk-2.1-sonarqube
-  ;;
-"dotnet-3-sdk-sonarqube")
-  ECR_REPO=dotnet-sdk
-  IMAGE_NAME=dotnet-sdk-3.1-sonarqube
-  ;;
-"dotnet-5-sdk-sonarqube")
-  ECR_REPO=dotnet-sdk
-  IMAGE_NAME=dotnet-sdk-5.0-sonarqube
-  ;;
-"node-8.15-jessie")
-  ECR_REPO=node
-  IMAGE_NAME=node-8.15-jessie-pip-awscli
-  ;;
-"node-12.16.2-buster")
-  ECR_REPO=node
-  IMAGE_NAME=node-12.16.2-buster-pip-awscli
-  ;;
-"dotnet-3.1-runtime")
-  ECR_REPO=dotnet-runtime
-  IMAGE_NAME=dotnet-runtime-3.1-pip-awscli
-  ;;
-"dotnet-5.0-aspnet")
-  ECR_REPO=aspnet
-  IMAGE_NAME=dotnet-aspnet-5.0-pip-awscli
-  ;;
+
 esac
 
-ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-ECR_URL=${ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com
+ECR_URL=mwlrancher01t.midwestlabs.com:5000
 IMAGE_TAG=${ECR_URL}/${ECR_REPO}:${IMAGE_NAME}
-DOCKERFILE=${WORKSPACE}/jobs/devops/custom-base-images/${IMAGE_NAME}.dockerfile
+# DOCKERFILE=${WORKSPACE}/jobs/devops/custom-base-images/${IMAGE_NAME}.dockerfile
+DOCKERFILE=${WORKSPACE}/${IMAGE_NAME}.dockerfile
 
 echo "Build ${DOCKER_IMAGE} docker image..."
 
@@ -90,9 +54,8 @@ echo "Build ${DOCKER_IMAGE} docker image..."
 docker build -f ${DOCKERFILE} -t ${IMAGE_NAME} .
 
 # Login, tag and push to ECR
-aws ecr get-login --no-include-email --region us-west-2 | sh
 docker tag ${IMAGE_NAME}:latest ${IMAGE_TAG}
-docker push ${IMAGE_TAG}
+# docker push ${IMAGE_TAG}
 docker rmi ${IMAGE_TAG}
 ''')
 	}
