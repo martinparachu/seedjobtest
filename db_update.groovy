9b8055381f6f6f93ca25bae35ed065da5afd6b93
martinparachu <51450877+martinparachu@users.noreply.github.com>
2021-01-18 16:19:13 -0300

    Update db_update.groovy

--- db_update.groovy
+++ db_update.groovy
@@ -1,4 +1,5 @@
 pipelineJob("db_update") {
+  disabled(true) // Disabled by default. Manually enable it when explicitly needed.
   parameters {
     choiceParam('target', ["stg", "val"], 'Target Database' )
     booleanParam('run_update', false, 'Check if you really want to proceed')
